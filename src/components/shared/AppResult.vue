cbecc3874c6f120a5b76b0f8ec5df5a266af0706
GustavoMKarnopp <gustavokarnopp.dev@gmail.com>
2021-09-14 18:07:05 -0300

    Finalizando estilização botão MOBILE/ DESKTOP

--- src/components/shared/AppResult.vue
+++ src/components/shared/AppResult.vue
@@ -10,8 +10,7 @@
         <v-row
         class="posicaoBTN"
         >
-          <v-btn
-            
+          <v-btn 
             @click.stop="drawer = !drawer"
           >
             Hist
@@ -38,10 +37,9 @@
             :key="item.title"
             link
           >
-  
-            <v-list-item-content>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item-content>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
           </v-list-item>
         </v-list>
       </v-navigation-drawer>
@@ -74,15 +72,24 @@ export default {
  @media (min-width: 320px){
    .posicaoBTN{
     max-width: 20%;
-    max-height:100px;
+    max-height: 100px;
     margin-right: 40px;
+    float: right;
+    margin: 0;
     position: relative;
-}
-     .estilBTN{
-    max-width: 20%;
-    max-height:100px;
-    margin-right: 40px;
-    position: relative;
+  }
+    .v-btn{
+    max-height: 32px !important;
+    color: #e6dfdf !important;
+    margin: -2px -6px 0px 0px;
+  }
+  .v-btn span{
+    font-size: 13px;
+    color: rgb(14, 11, 11) !important;
+  }
+  .theme--light.v-btn.v-btn--has-bg {
+    background-color:  #cfcdcd !important;
+    color: darkblue !important;
   }
  }
 
@@ -90,38 +97,53 @@ export default {
 
 
  @media (min-width: 768px){
-   .estilBTN{
-  height:60px;
-  max-width: 15%;
-  margin-left: 32%;
-  margin-right: 40px;
-  position: relative;
+    .v-btn{
+    max-height: 30px !important;
+    color:   #e6dfdf  !important;
+    margin: 0px
+  }
+  .v-btn span{
+   font-size: 13px;
+    color: rgb(14, 11, 11) !important;
+  }
+  .theme--light.v-btn.v-btn--has-bg {
+      background-color:  #cfcdcdb9 !important;
+      color: darkblue !important;
+  }
+    .posicaoBTN{
+    height:60px;
+    max-width: 20%;
+    max-height: 20%;
+    margin-left: 32%;
+    margin-right: 40px;
+    position: relative;
+  }
 }
-  .posicaoBTN{
-  height:60px;
-  max-width: 20%;
-  max-height: 20%;
-  margin-left: 32%;
-  margin-right: 40px;
-  position: relative;
-}
- }
 
 /*=========================================>> CONFIG. DESKTOP <<=========================================== */
 
 
-  @media (min-width: 1024px){  
-    .v-btn{
-      max-height: 30px !important;
-      color: brown !important;
-      margin: 0px
-    }
-    .posicaoBTN{
-      margin: 0;
-      margin-left: 33.1%;
-      margin-right: 65%;
-      position: relative;
-    }
+@media (min-width: 1024px){  
+  .v-btn{
+    max-height: 30px !important;
+    color:#e6dfdf  !important;
+    margin: 0px
+  }
+  .v-btn span{
+   font-size: 13px;
+    color: rgb(14, 11, 11) !important;
+  }
+.theme--light.v-btn.v-btn--has-bg {
+    background-color:  #cfcdcdb9 !important;
+    color: darkblue !important;
+}
+  
+.posicaoBTN{
+  margin: 0;
+  margin-left: 33.1%;
+      margin-right: 61%;
+  position: relative;
+}
  
 }
 
